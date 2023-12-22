037e8fab57bf311f5b0e5dca43bc0408180e7602
ujeani <jeong.woojin@gmail.com>
2023-12-22 19:22:49 +0900

    serialize/deserialize implementing

--- v2x_intf/v2x_subscriber.py
+++ v2x_intf/v2x_subscriber.py
@@ -1,15 +1,18 @@
 import rclpy as rclpy
 from rclpy.node import Node
+from rclpy.serialization import serialize_message, deserialize_message
+
 from case_msgs.msg import Recognition, Objects
 import socket
+
 obu_ip = '192.168.2.100' # TODO : MATCH IT!
 obu_port = 1000 # TODO : MATCH IT!
 
 # Create a TCP socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the server (establishing the connection)
-client_socket.connect((obu_ip, obu_port))
+# client_socket.connect((obu_ip, obu_port))
 
 class RecognitionSubscriber(Node):
     def __init__(self):
@@ -23,17 +26,19 @@ class RecognitionSubscriber(Node):
 
     def recognition_callback(self, msg):
         print("I heard:", msg)
+        serialized_data = serialize_message(msg)
+        # Print serialized msg
+        # client_socket.send() #* message.encode())
+        deserialized_msg = deserialize_message(Recognition, serialized_data)
+        print("serialize->deserialize : ", deserialize_message)
 
-        client_socket.send() #* message.encode())
-
-        # TODO : Send to OBU through TCP interface
 
 def main(args=None):
     rclpy.init(args=args)
     recognition_subscriber = RecognitionSubscriber()
     rclpy.spin(recognition_subscriber)
 
-    client_socket.close()
+    # client_socket.close()
     recognition_subscriber.destroy_node()
     rclpy.shutdown()
 
