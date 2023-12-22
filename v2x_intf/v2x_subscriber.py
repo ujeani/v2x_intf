eeccfcae47b40ee6336b98347fad79082c592d23
유리미쨩쨩쨩 <72840161+RohYuLim@users.noreply.github.com>
2023-12-22 16:25:50 +0900

    Update v2x_subscriber.py

--- v2x_intf/v2x_subscriber.py
+++ v2x_intf/v2x_subscriber.py
@@ -1,31 +1,33 @@
-# import rclpy as rclpy
-# from rclpy.node import Node
+import rclpy as rclpy
+from rclpy.node import Node
+from case_msgs.msg import Recognition, Objects
 
-# class RecognitionSubscriber(Node):
-#     def __init__(self):
-#         super().__init__('recognition_subscriber')
-#         self.subscription = self.create_subscription(
-#             # Recognition,
-#             'caselab/recognition',
-#             self.recognition_callback,
-#             10)
-#         self.subscription  # prevent unused variable warning
+class RecognitionSubscriber(Node):
+    def __init__(self):
+        super().__init__('recognition_subscriber')
+        self.subscription = self.create_subscription(
+            Recognition,
+            'caselab/recognition',
+            self.recognition_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-#     def recognition_callback(self, msg):
-#         # self.get_logger().info('I heard: "%s"' % msg.data)
-#         # Here we have to manipulate the recognition data
-#         print('I heard: "%s"' % msg.data)
+    def recognition_callback(self, msg):
+#             int32 vehicle_id
+# int32[] datetime
+# float32[] vehicle_pose
+# float32 vehicle_velocity
+# case_msgs/Objects[] object_data
 
-# def main(args=None):
-#     rclpy.init(args=args)
-#     recognition_subscriber = RecognitionSubscriber()
-#     rclpy.spin(recognition_subscriber)
+        self.get_logger().info('I heard: "%s"' % msg.vehicle_id)
 
-#     recognition_subscriber.destroy_node()
-#     rclpy.shutdown()
+def main(args=None):
+    rclpy.init(args=args)
+    recognition_subscriber = RecognitionSubscriber()
+    rclpy.spin(recognition_subscriber)
 
-def main():
-    print('Hi from v2x_subscriber.')
+    recognition_subscriber.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
