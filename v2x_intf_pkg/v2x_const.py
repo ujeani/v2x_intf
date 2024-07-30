5e0630eefea24ad9128cbc3d1f53eab6cbab9939
Woojin Jeong <woojin@wapplecloud.com>
2024-07-30 17:24:29 +0900

    Update v2x_const.py

--- v2x_intf_pkg/v2x_const.py
+++ v2x_intf_pkg/v2x_const.py
@@ -3,13 +3,13 @@ class V2XConstants:
     OBU_PORT = 9201
     R = 6378137.0 # Radius of the Earth in meters
 
-    fmsgHdrType = f'<III'
+    fmsgHdrType = '<III'
     fDDateTimeType = 'HBBBBHh'  # year, month, day, hour, minute, second, microsecond
     fPosition3D = 'll'          # (latitude, longitude)
     fPositionalAccuracy = 'BBH'
     fFirstPart = f'<B {fDDateTimeType} {fPosition3D} {fPositionalAccuracy} B' # equipmentType, sDSMTimeStamp, refPos, refPosXYConf, numDetectedObjects
     fDetectedObjectCommonData = '<BBHhBhhBHBHB'
-    HDR_FLAG = 0x53415445,         # hdr
-    MSG_RECOGNITION = 0x16792,           # msgID for recognition
+    HDR_FLAG = 0x53415445
+    MSG_RECOGNITION = 0x016792
 
     
