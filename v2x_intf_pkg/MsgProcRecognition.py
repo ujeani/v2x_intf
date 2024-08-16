f5e9b415430f7c5fa624cf3219cfc0d4139bcad4
ujeani <jeong.woojin@gmail.com>
2024-08-16 13:34:50 +0900

    Update MsgProcRecognition.py

--- v2x_intf_pkg/MsgProcRecognition.py
+++ v2x_intf_pkg/MsgProcRecognition.py
@@ -280,6 +280,8 @@ class MsgProcRecognition:
     self.logger.info(f'(ROS->): hdr size {ctypes.sizeof(fmtcommon.v2x_intf_hdr_type)}, fixed part size {fixed_part_size}, objects size {objects_size}')
     recog_msg.hdr.msgLen = fixed_part_size + objects_size
     self.logger.info(f'(ROS->): msgLen {recog_msg.hdr.msgLen}')
+    self.logger.info(f'(ROS->): object_array size len {len(objects_array)}')
+    self.logger.info(f'(ROS->): recog_msg.objects len {len(recog_msg.objects)}')
     self.logger.info(f'(ROS->): recognition message created length {ctypes.sizeof(recog_msg)}')
     self.logger.info(f'(ROS->): recognition message created {bytes(recog_msg)}')
     self.logger.info(f'(ROS->): recognition message created with lenght {len(bytes(recog_msg))}')
